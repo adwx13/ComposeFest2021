f9d2eb0969a6445014b6faa9765925964f6bb0b2
sungjin <dawx14@naver.com>
2021-11-14 00:09:24 +0900

    Scaffold, modifier 사용

--- week 2-1-Layouts in JetpackCompose/week 2/app/src/main/java/pri/sungjin/layoutinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/week 2/app/src/main/java/pri/sungjin/layoutinjetpackcompose/MainActivity.kt
@@ -3,24 +3,32 @@ package pri.sungjin.layoutinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pri.sungjin.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LayoutInJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            PhotographerCard()
         }
     }
 }
@@ -31,13 +39,58 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun PhotographerCard() {
-    Column {
-        Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
-        
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable { }
+        .padding(16.dp)) {
+        Surface(
+           modifier = Modifier.size(50.dp),
+           shape = CircleShape,
+           color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {  }
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
+    }
+}
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(text = "LayoutsCodelab")
+        }, actions = {
+            IconButton(onClick = {  }) {
+                Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+            }
+        })
+    }) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutInJetpackComposeTheme {
+        LayoutsCodelab()
     }
 }
 
