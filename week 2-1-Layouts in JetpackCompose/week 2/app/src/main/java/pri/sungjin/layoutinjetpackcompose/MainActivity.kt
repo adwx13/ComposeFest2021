e98458fba41e08fdafc146f3ace8fbab9d41a091
sungjin <dawx14@naver.com>
2021-11-15 21:25:32 +0900

    decoupled Constraint 사용

--- week 2-1-Layouts in JetpackCompose/week 2/app/src/main/java/pri/sungjin/layoutinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/week 2/app/src/main/java/pri/sungjin/layoutinjetpackcompose/MainActivity.kt
@@ -21,10 +21,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import androidx.constraintlayout.solver.widgets.Rectangle
 import coil.compose.rememberImagePainter
 import kotlinx.coroutines.launch
@@ -249,9 +253,96 @@ fun Chip(modifier: Modifier = Modifier, text: String) {
     }
 }
 
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        val (button1, button2, text) = createRefs()
+
+        Button(onClick = {  },
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }) {
+            Text("Button")
+        }
+
+        Text("Text", Modifier.constrainAs(text) {
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerHorizontallyTo(parent)
+        })
+
+        val barrier = createEndBarrier(button1, text)
+        Button(onClick = { }, modifier = Modifier.constrainAs(button2){
+            top.linkTo(parent.top, margin = 16.dp)
+            start.linkTo(barrier)
+        }) {
+            Text("Button 2")
+        }
+    }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+    ConstraintLayout {
+        val text = createRef()
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text("This is a very very very very very very very very long text", modifier = Modifier.constrainAs(text) {
+            linkTo(start = guideline, end = parent.end)
+            width = Dimension.preferredWrapContent
+        })
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp)
+        } else {
+            decoupledConstraints(margin = 32.dp)
+        }
+
+        ConstraintLayout(constraints) {
+            Button(onClick = { }, modifier = Modifier.layoutId("button")) {
+                Text("Button")
+            }
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin = margin)
+        }
+    }
+}
+
 
 //Preview -------------------------------------------------------------------------
 
+@Preview
+@Composable
+fun LargeConstraintLayoutPreview() {
+    LayoutInJetpackComposeTheme {
+        LargeConstraintLayout()
+    }
+}
+
+
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+    LayoutInJetpackComposeTheme {
+        ConstraintLayoutContent()
+    }
+}
 
 @Preview
 @Composable
